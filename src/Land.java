1d95705499afc8462ff8cd153188262f0d59b5ad
Aaron Stout <arstout@gmail.com>
2014-06-04 21:13:39 -0400

    Added some more classes

---
+++ src/Land.java
@@ -0,0 +1,9 @@
+
+public class Land {
+    private String biome     = "";
+    private int    elevation = 0;
+    private Structure[] structures;
+    private Person[]    persons;
+    private Creature[]  creatures;
+    
+}
